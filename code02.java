41d652006a6bafc8a57d60ebd067524eb8a0644e
naman0001 <94049418+naman0001@users.noreply.github.com>
2022-03-10 17:46:52 +0530

    Add files via upload

---
+++ code02.java
@@ -0,0 +1,19 @@
+class calculator
+{
+
+    public static void main( String[] args)
+{
+
+          int a=20;
+          int b=10;
+          int sum= a+b;
+          int subs=b-a;
+          int multiply= a*b;
+          int division=a/b;
+
+System.out.println(" sum is: " + sum); 
+System.out.println(" subs is: "+ subs); 
+System.out.println(" multiply is: " +multiply); 
+System.out.println(" division is: " +division);
+}
+} 
